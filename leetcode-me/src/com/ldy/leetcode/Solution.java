f97e828e9d495c9ceb09c30af917d62ee1234f22
lidyang <lidgyang@foxmail.com>
2021-11-08 13:39:08 +0800

    299. 猜数字游戏

--- leetcode-me/src/com/ldy/leetcode/Solution.java
+++ leetcode-me/src/com/ldy/leetcode/Solution.java
@@ -1,8 +1,6 @@
 package com.ldy.leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author lidongyang
@@ -15,11 +13,47 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[][] nums = new int[][]{{2, 2}, {3, 3}};
-        int ans = solution.maxCount(5,4,nums);
+//        int[][] nums = new int[][]{{2, 2}, {3, 3}};
+//        int ans = solution.maxCount(5,4,nums);
+
 
 
     }
+
+    public String getHint(String secret, String guess) {
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        char[] secretChars = secret.toCharArray();
+        char[] guessChars = guess.toCharArray();
+        int count = 0;
+        for(Character c : secretChars){
+            if(map.containsKey(c)){
+                map.replace(c,map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for(Character c : guessChars){
+            if(map.containsKey(c)){
+                count++;
+                if(map.get(c) == 1){
+                    map.remove(c);
+                }else{
+                    map.replace(c,map.get(c) - 1);
+                }
+                continue;
+            }
+        }
+        int aNumber = 0;
+        for(int i=0;i<secretChars.length;i++){
+            if (secretChars[i] == guessChars[i]){
+                aNumber++;
+            }
+        }
+        int bNumber = count - aNumber;
+        return aNumber+"A"+bNumber+"B";
+
+    }
+
 
     public int maxCount(int m, int n, int[][] ops) {
 
